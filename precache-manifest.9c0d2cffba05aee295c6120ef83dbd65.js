1a993a0fb61c97614a5f92ac854d6f9f016bbc3d
Reidar Liabø <reidar.liabo@gmail.com>
2020-02-29 23:38:40 +0100

    Updates

---
+++ precache-manifest.9c0d2cffba05aee295c6120ef83dbd65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ffb0452d3ac36eaa7a4880f4afdff7b",
+    "url": "/bouvet-react-lib/index.html"
+  },
+  {
+    "revision": "b556445bca0e3dd3ae9a",
+    "url": "/bouvet-react-lib/static/css/main.a2b7b0b6.chunk.css"
+  },
+  {
+    "revision": "1267c0bc1aec48f11fc4",
+    "url": "/bouvet-react-lib/static/js/2.0abd28ba.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/bouvet-react-lib/static/js/2.0abd28ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b556445bca0e3dd3ae9a",
+    "url": "/bouvet-react-lib/static/js/main.9edb28b9.chunk.js"
+  },
+  {
+    "revision": "5ae2573baf7646bff22c",
+    "url": "/bouvet-react-lib/static/js/runtime-main.6bd56488.js"
+  }
+]);
